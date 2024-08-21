437dca1bc6a24df727093b297de14106e6e49a6a
Prateep Ruangritkul <prateep.r@arise.tech>
2024-08-21 16:13:14 +0700

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
             },
             onError: (error) => {
                 console.log("error:", error)
-                setShowAccessToken(error.message + " [" + error.code + "]")
+                setShowCustomer(error)
             },
         });
 
